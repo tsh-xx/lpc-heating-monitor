b6732143b8cd2f9d18046012aadcf15232f9a56d
tsh.pa2@gmail.com <tsh.pa2@gmail.com>
2012-09-29 17:39:01 +0100

    Add more tuning parameters, make oil temp sense less jittery

--- README.md
+++ README.md
@@ -2,3 +2,26 @@ lpc-heating-monitor
 ===================
 
 Heating monitor and control system for LPC1769 dev board
+
+Uses the LPC-xpresso dev board and the mbed and CMSIS libraries.
+
+Libraries from mbed.com:
+=======================
+mbed-export rev 0, 22 Feb 2012
+DS18S20     rev 1, 15 Dec 2011
+USBDevice   rev 0, 30 May 2012
+
+Connect one-wire devices to mbed pin 9 (with active power)
+Connect LED sense circuit to mbed pin 9
+Connect USB device for control/capturing data
+
+Temperature sensors use the on-board EEPROM to identify which position to report in.
+This means that missing devices don't affect the logging.
+
+Electricity consumption is logged using an IRQ to sample timer intervals between flashes.
+At the moment, there is no filtering on any of the sensors.
+
+Readings are printed every 30 sec, or 5 sec in verbose mode.
+
+
+
--- main.cpp
+++ main.cpp
@@ -2,7 +2,9 @@
 #include "USBSerial.h"
 #include "DS1820.h"
 
+//p9 is temperature bus
 DigitalOut led(LED1);
+DigitalOut suppress(p10);
 AnalogIn adc(p18);
 InterruptIn meter_pulse(p8);
 DigitalIn pulse(p8);
@@ -23,9 +25,16 @@ enum {WAITING,
 	TIME,
 	DATE,
 	SET_PORT,
-	SET_ID
+	SET_ID,
+	SET_FLOOR,
+	SET_DAY,
+	SET_MIN,
+	SET_NIGHT
 };
-enum {QUIET, VERBOSE};
+enum {QUIET, VERBOSE, DHW};
+enum {COLD, RELAX, WARM, RELAX_MISS, WARM_MISS};
+enum {NIGHT, NIGHTBOOST, E7, SHORT, LONG, DAY};
+
 
 #define MAX_PROBES 9
  DS1820* probe[MAX_PROBES];
@@ -33,6 +42,7 @@ enum {QUIET, VERBOSE};
  void countPulse() {
 	 int current_time;
 	 meter_pulses++;
+         // Only use the first pulse before it gets sampled later for power calculations
 	 if(meter_pulses == 1) {
 		 current_time = t.read_ms();
 		 if (current_time > meter_previous) {
@@ -59,12 +69,32 @@ enum {QUIET, VERBOSE};
      int seq[MAX_PROBES];
      int seq_new[MAX_PROBES];
      int interval = 30;
+	 int current_time;
+	 int dhw_always = 0;
+	 int dhw_floor = 38;
+	 int dhw_am = 48;
+	 int dhw_day = 45;
+	 int dhw_recharge = 25;
+//	 int dhw_setpoint = 45;
+	 float low[5];
+	 float oil;
+	 float top;
+	 int oil_state = COLD;
+	 int dhw_state = DAY;
+	 const char oil_char[5] = {'c','r','w','R','W'};
+	 const char dhw_char[6] = {'n','N','7','S','L','d'};
+     const char suppress_char[2] = {'-','*'};
 
      // See if this makes the USB more stable.
      wait_us(500);
 
      serial.printf("Heating Control\r");
 
+     suppress = 0;
+
+     for(i=0;i<5;i++){
+    	 low[i]=10;
+     }
      meter_pulse.fall(&countPulse);
      t.start();
      reading_interval.start();
@@ -121,6 +151,11 @@ enum {QUIET, VERBOSE};
         		 break;
         	 case 'i':{
         		 serial.printf("Heating Control on Cortex-M3\n");
+        		 serial.printf("Hot Water enable: %d\n",dhw_always);
+        		 serial.printf("Hot Water daytime floor  : %d\n",dhw_floor);
+        		 serial.printf("Hot Water mornint preheat: %d\n",dhw_am);
+        		 serial.printf("Hot Water daytime targer : %d\n",dhw_day);
+        		 serial.printf("Hot Water min lower temp : %d\n",dhw_recharge);
         		 break;
         	 }
         	 case 't': {
@@ -209,7 +244,61 @@ enum {QUIET, VERBOSE};
             			 }
             			 }
             			 break;
-        		 default:
+                		 case SET_FLOOR:
+                			 input_count++;
+                			 serial.putc(command);
+                			 switch(input_count){
+                			 case 1: in_hold = command - '0'; break;
+                			 case 2: {
+                				 dhw_floor = (in_hold * 10 + (command - '0'));
+                				 parse_state = WAITING;
+                				 input_count = 0;
+                             	 serial.printf("\nHot Water daytime floor set to %d\n", dhw_floor);
+                			 break;
+                			 }
+                			 }
+                			 break;
+                    		 case SET_DAY:
+                    			 input_count++;
+                    			 serial.putc(command);
+                    			 switch(input_count){
+                    			 case 1: in_hold = command - '0'; break;
+                    			 case 2: {
+                    				 dhw_day = (in_hold * 10 + (command - '0'));
+                    				 parse_state = WAITING;
+                    				 input_count = 0;
+                                 	 serial.printf("\nHot Water daytime target set to %d\n", dhw_day);
+                    			 break;
+                    			 }
+                    			 }
+                    			 break;                		 case SET_MIN:
+                        			 input_count++;
+                        			 serial.putc(command);
+                        			 switch(input_count){
+                        			 case 1: in_hold = command - '0'; break;
+                        			 case 2: {
+                        				 dhw_recharge = (in_hold * 10 + (command - '0'));
+                        				 parse_state = WAITING;
+                        				 input_count = 0;
+                                     	 serial.printf("\nHot Water nighttime floor set to %d\n", dhw_recharge);
+                        			 break;
+                        			 }
+                        			 }
+                        			 break;                		 case SET_NIGHT:
+                            			 input_count++;
+                            			 serial.putc(command);
+                            			 switch(input_count){
+                            			 case 1: in_hold = command - '0'; break;
+                            			 case 2: {
+                            				 dhw_am = (in_hold * 10 + (command - '0'));
+                            				 parse_state = WAITING;
+                            				 input_count = 0;
+                                         	 serial.printf("\nHot Water night setpoint set to %d\n", dhw_am);
+                            			 break;
+                            			 }
+                            			 }
+                            			 break;
+                            			 default:
         			 serial.printf("Got digit, but no use for it '%c'\n",command);
         			 break;
         	}
@@ -220,9 +309,15 @@ enum {QUIET, VERBOSE};
         		 serial.printf("Normal verbosity\n");
         		 break;
         	 case 'V':
+        		 if (verbosity == DHW){
         		 verbosity = VERBOSE;
         		 interval = 5;
         		 serial.printf("Debug verbosity\n");
+        		 } else {
+            		 verbosity = DHW;
+            		 interval = 30;
+            		 serial.printf("Hot Water verbosity\n");
+        		 }
         		 break;
         	 case '?':
         		 serial.printf("v/V : Normal/High verbosity\n");
@@ -231,6 +326,11 @@ enum {QUIET, VERBOSE};
         		 serial.printf("i   : Information\n");
         		 serial.printf("e   : Enumerate 1-wire devices\n");
         		 serial.printf("s   : Set thermometer identifier\n");
+        		 serial.printf("h/H : Hot water control active\n");
+        		 serial.printf("f   : Set DHW floor daytime (38)\n");
+        		 serial.printf("F   : Set DHW daytime charge (45)\n");
+        		 serial.printf("n   : Set DHW floor nighttime (25)\n");
+        		 serial.printf("N   : Set DHW overnight charge (48)\n");
         		 break;
         	 case 'e':
       			serial.printf("\nChecking Thermometers\n\n");
@@ -251,6 +351,31 @@ enum {QUIET, VERBOSE};
         			serial.printf("\n");
         		}
         		break;
+        	 case 'h':
+        		 dhw_always = 1;
+        		 serial.printf("Hot water always enabled\n");
+        		 break;
+        	 case 'H':
+        		 dhw_always = 0;
+        		 serial.printf("Hot water timer control active\n");
+        		 break;
+        	 case 'f':
+        		 parse_state = SET_FLOOR;
+        		 serial.printf("Enter daytime DHW floor\n");
+        		 break;
+        	 case 'F':
+        		 parse_state = SET_DAY;
+        		 serial.printf("Enter daytime DHW target\n");
+        		 break;
+        	 case 'n':
+        		 parse_state = SET_MIN;
+        		 serial.printf("Enter night DHW floor\n");
+        		 break;
+        	 case 'N':
+        		 parse_state = SET_NIGHT;
+        		 serial.printf("Enter night heat target\n");
+        		 break;
+
         	 case 's':
         		 parse_state = SET_PORT;
         		 serial.printf("Enter Thermometer index\n");
@@ -288,12 +413,13 @@ enum {QUIET, VERBOSE};
              		 if (interval < 15) {
              			 interval += 30;
              		 }
+                         if (meter_rate > 2500 and interval > 15) {
+                             interval = 15;
+                         }
              	 }
              	 // Detect midnight
              	 if (buffer[0]=='0' && buffer[1]=='0' && buffer[2]=='0' && buffer[3]=='0' && buffer[4]=='0'){
              		clean_pulses = 0;
-                	 strftime(buffer_date, 10, "%y%m%d", localtime(&seconds));
-                	 serial.printf(" New Day %s\n%s ", buffer_date, buffer);
              	 }
 
 
@@ -303,21 +429,207 @@ enum {QUIET, VERBOSE};
              		 if(temp > devices_found){
              			serial.printf("-00.00 ");
              		 } else {
-             			serial.printf("%2.2f ",probe[seq[i]]->temperature('c'));
+             			serial.printf("% 2.2f ",probe[seq[i]]->temperature('c'));
              		 }
              	 }
+                 // Check that rate is still accurate
+                 // Doesn't handle wrap-round case...
+		 current_time = t.read_ms();
+		 if ((current_time > (meter_previous + meter_interval)) && (current_time > meter_previous)) {
+			 meter_interval = current_time - meter_previous;
+                         meter_rate = 3600000.0 / meter_interval;
+		         // Clip bad numbers
+                         if (meter_rate > 4800) {
+                             meter_rate = 4803;
+                         }
+		 }
 
-             	 serial.printf(" : %6.1f %5d\n", meter_rate, clean_pulses);
+             	 serial.printf(" : %6.1f %5d", meter_rate, clean_pulses);
+
+             	 serial.printf(" %c%c%c %.1f\n",oil_char[oil_state],dhw_char[dhw_state],suppress_char[suppress],low[4]-low[0]);
+             	 if(verbosity != QUIET){
+             		 serial.printf("DHW: Top:% 2.2f Low:% 2.2f Rate: % 2.2f\n",top,low[0],low[4]-low[0]);
+             	 }
+
+
+             	 // dump other temp sensors too
              	 i=0;
              	 while(i<devices_new){
              		 serial.printf("%2.2f ",probe[seq_new[i]]->temperature('c'));
              		 i++;
              	 }
+            	 //Pipeline low temperatures
+            	low[4] = low[3];
+            	low[3] = low[2];
+            	low[2] = low[1];
+            	low[1] = low[0];
         	 }
 
+
+        	 // Process oil heater state
+        	 if(seq[6] == MAX_PROBES+1){
+        		 oil=48;
+        	 } else {
+        		 oil =  probe[seq[6]]->temperature('c');
+        		 if(oil<35) {
+        			 oil =  probe[seq[6]]->temperature('c');
+        		 }
+        		 }
+        	 switch (oil_state){
+        	 case COLD:
+        		 if(oil > 47) {
+    				 serial.printf("Was cold, passed 47 to reheat\n");
+        			 oil_state = RELAX;
+        		 } else
+        		 {
+        			 if (suppress == 0 and dhw_always == 0){
+        				 serial.printf("Reached Cold state - suppressing DHW\n");
+        				 suppress = 1;
+        			 }
+        		 }
+        		 break;
+        	 case RELAX:
+        		 if(oil < 42 && oil > 40) {
+    				 serial.printf("Heat cycle done\n");
+    				 oil_state = WARM;}
+        		 if(oil < 36) {
+    				 serial.printf("Failed to hear up %f\n",oil);
+    				 oil_state = RELAX_MISS;}
+        		 break;
+        	 case WARM:
+        		 if(oil < 32) {
+        			 oil_state = WARM_MISS;
+    				 serial.printf("Was warm, now cold @ %f\n",oil);
+        		 }
+        		 break;
+        	 case RELAX_MISS:
+        		 if(oil > 36) {
+    				 serial.printf("Recovered\n");
+    				 oil_state = RELAX;}
+        		 if(oil < 34) {
+    				 serial.printf("Still Failed to hear up %f\n",oil);
+    				 oil_state = COLD;}
+        		 break;
+        	 case WARM_MISS:
+        		 if(oil>33) {
+        			 oil_state = WARM;
+    				 serial.printf("Warm again @ %f\n",oil);
+        		 }
+        		 if(oil < 31) {
+        			 oil_state = COLD;
+    				 serial.printf("Was warm, now cold @ %f\n",oil);
+        		 }
+        		 break;
+        	 }
+
+        	 // Process Water heater state
+        	 // Probe 0 = top
+        	 // Probe 2 = Flow
+        	 // Probe 1 = Lower
+
+        	 if(seq[1] == MAX_PROBES+1){
+        		 low[0] = 20;
+        	 } else {
+        		 low[0] = probe[seq[1]]->temperature('c');
+        	 }
+        	 if(seq[0] == MAX_PROBES+1){
+        		 	 top = 40;
+        	 } else {
+        		 top =  probe[seq[0]]->temperature('c');
+        	 }
+        	 switch (dhw_state){
+        	 // 9pm to 8am (or heat cycle done)
+        	 case NIGHT:
+        		 if(dhw_always == 0) {
+        			 suppress = 1;
+        		 }
+        		 // Try to catch end of oil cool-down cycle
+             	 if (buffer[0]=='0' && buffer[1]=='6' &&(oil < 42 || buffer[2]>'3')) {dhw_state = E7;}
+             	 if (buffer[0]=='1' || buffer[1]>'8')  {dhw_state = DAY;}
+             	 // Too cold, short heat
+             	 if ((low[0] < dhw_recharge) && (oil_state == WARM)) {
+             		 suppress = 0;
+             		dhw_state = NIGHTBOOST;
+             	 }
+             	 break;
+        	 case NIGHTBOOST:
+        		 suppress = 0;
+             	 if (top > dhw_day and dhw_always == 0) {
+             		 suppress = 1;
+             		 dhw_state = NIGHT;
+             	 }
+        		 if(oil_state != WARM and dhw_always == 0) {
+        			 suppress = 1;
+        		     dhw_state = NIGHT;
+        		 }
+        		 break;
+        	 case E7:
+        		 suppress = 0;
+             	 if (top > dhw_am and dhw_always == 0) {
+             		 suppress = 1;
+             		 dhw_state = DAY;
+             	 }
+        		 if(oil_state != WARM and dhw_always == 0) {
+        			 suppress = 1;
+        		     dhw_state = NIGHT;
+        		 }
+        		 break;
+        	 case SHORT:
+        		 if ((top > dhw_day) and ((low[0]-low[4]) > .25) and dhw_always == 0) {
+        			 suppress = 1;
+        		     dhw_state = DAY;
+        		 }
+        		 if(oil_state != WARM and dhw_always == 0) {
+        			 suppress = 1;
+        		     dhw_state = DAY;
+        		 }
+        		 break;
+        	 case LONG:
+        		 if (top > dhw_am and dhw_always == 0) {
+        			 suppress = 1;
+        			 dhw_state = DAY;
+        		 }
+        		 if(oil_state != WARM and dhw_always == 0) {
+        			 suppress = 1;
+        		     dhw_state = DAY;
+        		 }
+        		 break;
+        	 case DAY:
+        		 if(dhw_always == 0) {
+        			 suppress = 1;
+        		 }
+        		 // 10pm is night, no need for any hot water.
+             	 if (buffer[0]=='2' &&  buffer[1]>'1' and dhw_always == 0)  {
+             		 dhw_state = NIGHT;
+             		 suppress = 1;
+             		 break;
+             	 }
+             	 if (buffer[0]=='0' &&  buffer[1]<'6' and dhw_always == 0)  {
+             		 dhw_state = NIGHT;
+             		 suppress = 1;
+             		 break;
+             	 }
+             	 if ((((low[4]-low[0]) > 1) ||(top < dhw_floor)) && (oil_state == WARM)) {
+             		 dhw_state = SHORT;
+             		 suppress = 0;
+             	 }
+             	 if ((low[0]< dhw_recharge) && (oil_state == WARM)) {
+             		 dhw_state = LONG;
+             		 suppress = 0;
+             	 }
+        		 break;
+        	 }
+
+        	 if (dhw_always == 1){
+        		 suppress = 0;
+        	 }
         	 // Idle loop processing of meter reading
              if (meter_pulses){
             	 meter_rate = 3600000.0 / meter_interval;
+		 // Clip bad numbers
+                 if (meter_rate > 4800) {
+                     meter_rate = 4803;
+                 }
             	 led = !led;
             	 if(verbosity==VERBOSE){
             	 serial.printf("%d Pulse interval %dus, %4.3f W\n",meter_pulses, meter_interval,meter_rate);
